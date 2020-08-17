9f8d2a85450b47b42ffacf36485102be3b4a370c
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 21:35:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,20 +1,22 @@
 # code here!
 require 'pry'
 class School
-  attr_reader :name, :roster
   #@roster = {}
   #had to define the setter ourselves because this is not a standard property, cna't use attr
-
+  #super interesting: if i put @roster here, the method won't be able to access it...
+  #i've seen people putting class constants here, but i guess not instance variables....
+  #note: if we use attr stuff you're creating methods that WILL create new instance variables ONLY when the methods are called
+  #they're not created at first. 
 
   def initialize(name)
     @name = name
     @roster = {}
+    #had to define the setter ourselves because this is not a standard property, cna't use attr
+
   end
 
 def add_student(studentname, grade)
   #create new grade if neccessary
-  binding.pry
-
   if !@roster.keys.include?(grade)
     @roster[grade] = []
   end
