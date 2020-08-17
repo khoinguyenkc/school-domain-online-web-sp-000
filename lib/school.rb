c53862e0e9267fc9fe48e65bacdfc99559291e9d
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 21:10:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+
+class School
+  @roster = {}
+  #had to define it ourselves because this is not a standard property, cna't use attr
+
+
+def initialize(name)
+  @name = name
+end
+
+
+
+
+
+
+
+end
