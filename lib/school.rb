8631906e544bea352eb64a68e6142c4fd7271cec
Vietkhoi Nguyen <khoinguyenkc@gmail.com>
2020-08-17 21:14:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,16 @@
 # code here!
 
 class School
-  attr_reader :name
+  attr_reader :name, :roster
   @roster = {}
-  #had to define it ourselves because this is not a standard property, cna't use attr
+  #had to define the setter ourselves because this is not a standard property, cna't use attr
 
 
 def initialize(name)
   @name = name
 end
+
+
 
 
 
